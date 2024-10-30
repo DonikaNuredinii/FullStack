2f9b8935a07c9a270cd74cd7902c012e1f674da2
DonikaNuredinii <donikanuredini3@gmail.com>
2024-10-30 11:27:08 +0100

    mediascreen,javascipt

---
+++ lesson22/script.js
@@ -0,0 +1,21 @@
+console.log("Goodbye");
+
+var x=10;
+var _value="Digital school";
+console.log(x)
+// var 398745="ewr"; this is not how to do
+//var *gseegr=ssd; not to start with * 
+var y=100;
+var z=x+y;
+var i=101.0;
+console.log(x==y);
+console.log(x===z);
+var emri="Donika"
+var mbiemri="Nuredini"
+console.log(emri+ " "+mbiemri)
+
+
+var school="DS"
+console.log(`Hello from ${school}`)
+
+console.log( x==10 && y==10)
