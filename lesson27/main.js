8eea1ce38a95ce14fff167341e6948899da8dc12
DonikaNuredinii <donikanuredini3@gmail.com>
2024-11-04 12:10:26 +0100

    conditionals

---
+++ lesson27/main.js
@@ -0,0 +1,29 @@
+var x = 10;
+var y = 5;
+
+if(x>y){
+    console.log("this is true");
+}else{
+    console.log("this is wrong");
+}
+
+
+
+if(x<y){
+    console.log("this is wrong");
+}else{
+    console.log("this is true");
+}
+
+
+if(y>x){
+    console.log("this is true");
+}else{
+    console.log("this is wrong");
+}
+
+if(x==7){
+    console.log("10=7");
+}else{
+    console.log("10 is not equal 7")
+}
