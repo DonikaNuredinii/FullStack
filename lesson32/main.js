450ededcd02aa14781f3a2a0b88883fb8fd05552
DonikaNuredinii <donikanuredini3@gmail.com>
2024-11-11 12:30:37 +0100

    game

--- lesson32/main.js
+++ lesson32/main.js
@@ -7,20 +7,54 @@
 // }
 // printName();
 
-var colors = ['red', 'green', 'blue', 'purple'];
+// var colors = ['red', 'green', 'blue', 'purple'];
 
-function changeColor(){
-    document.querySelector('body').style.background = 
-    colors[Math.floor(Math.random()*colors.length)];
+// function changeColor(){
+//     document.querySelector('body').style.background = 
+//     colors[Math.floor(Math.random()*colors.length)];
+// }
+// changeColor();
+
+// var names = ['Antigona' , 'Grenti' , 'Donika'];
+
+// function changeName(){
+//     document.querySelector('p').innerHTML = 
+//     names[Math.floor(Math.random()*names.length)];
+// }
+
+// setInterval(changeColor, 1000);
+// setInterval(changeName, 2000);
+
+//////circle
+
+var circle = document.getElementById("circle");
+var showTime = document.getElementById("time");
+var timeStart = new Date().getTime();
+
+function showCircle() {
+    circle.style.display = "block";
+    circle.style.top = Math.random() * 500 + "px";
+    circle.style.left = Math.random() * 900 + "px";
+    circle.style.backgroundColor = randomColor();  
+    timeStart = new Date().getTime();
 }
-changeColor();
+showCircle();
 
-var names = ['Antigona' , 'Grenti' , 'Donika'];
-
-function changeName(){
-    document.querySelector('p').innerHTML = 
-    names[Math.floor(Math.random()*names.length)];
+circle.onclick = function() {
+    circle.style.display = "none";
+    setTimeout(showCircle, 1000); 
+    var timeEnd = new Date().getTime();
+    var time = (timeEnd - timeStart) / 1000;
+    showTime.innerHTML = time + 's';
 }
 
-setInterval(changeColor, 1000);
-setInterval(changeName, 2000);
+function randomColor() {
+    var s = '0123456789ABCDEF';
+    var color = "#";
+
+    for (var i = 0; i < 6; i++) {
+        color = color + s[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
